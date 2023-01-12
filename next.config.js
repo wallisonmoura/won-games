86d0e153c9134064e7ef1e3512968f3a1631b6d1
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-12 19:48:56 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+})
+const nextConfig = withPWA({
+  reactStrictMode: true
+})
+
+module.exports = nextConfig
