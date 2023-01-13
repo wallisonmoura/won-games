0cdb6aa0d49b8c293b09453717235b956d796816
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-13 12:33:30 -0300

    feat: Configuration of themes and styled components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
+import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../src/styles/global'
+import theme from '../src/styles/theme'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Story />
-    </>
+    </ThemeProvider>
   )
 ]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React - NextJS</title>
         <link rel="shortcut icon" href="img/icon-512.png" />
@@ -17,6 +19,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
