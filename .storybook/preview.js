c36a40a8c422a1ded1135779316353da8d3eee1c
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-13 14:39:24 -0300

    adjust storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-import GlobalStyle from '../src/styles/global'
-import theme from '../src/styles/theme'
+import GlobalStyle from 'styles/global'
+import theme from 'styles/theme'
 
 export const decorators = [
   (Story) => (
