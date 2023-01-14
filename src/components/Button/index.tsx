9430b43495f627553048aae8a20bcf47f160b3f5
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-14 20:03:22 -0300

    feat: button component with tests and storybook.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => {
+  return (
+    <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+      {icon}
+      {!!children && <span>{children}</span>}
+    </S.Wrapper>
+  )
+}
+
+export default Button
