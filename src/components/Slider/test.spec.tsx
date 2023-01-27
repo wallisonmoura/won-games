851751229eccaab07a6666426a3284f813783138
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-26 21:42:54 -0300

    feat: Create tests and match-media-mock to slider

--- src/components/Slider/test.spec.tsx
+++ src/components/Slider/test.spec.tsx
@@ -1,11 +1,24 @@
-import { screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen, render } from '@testing-library/react'
 
 import Slider from '.'
 
 describe('<Slider />', () => {
   it('render test', () => {
-    // const { container } = render(<Slider />)
-    // expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
-    // expect(container.firstChild).toMatchSnapshot()
+    const { container } = render(
+      <Slider settings={{ slidesToShow: 1, infinite: false }}>
+        <p>item 1</p>
+        <p>item 2</p>
+      </Slider>
+    )
+
+    expect(
+      screen.getByText(/item 1/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+    expect(
+      screen.getByText(/item 2/i).parentElement?.parentElement
+    ).toHaveClass('slick-slide')
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
