1eee132a81f2f7d0db2b58fc97d811a8908f16dc
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-26 21:22:43 -0300

    feat: Slider component created.

---
+++ src/components/Slider/test.spec.tsx
@@ -0,0 +1,11 @@
+import { screen } from '@testing-library/react'
+
+import Slider from '.'
+
+describe('<Slider />', () => {
+  it('render test', () => {
+    // const { container } = render(<Slider />)
+    // expect(screen.getByRole('heading', { name: /Slider/i })).toBeInTheDocument()
+    // expect(container.firstChild).toMatchSnapshot()
+  })
+})
