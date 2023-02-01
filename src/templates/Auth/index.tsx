a3be02b3135cd280000ad331b782895ca997849d
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-02-01 13:26:10 -0300

    Add Auth Desktop styles

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,9 +1,41 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
 import * as S from './styles'
 
-const Auth = () => {
+export type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => {
   return (
     <S.Wrapper>
-      <h1>Auth</h1>
+      <S.BannerBlock>
+        <S.BannerContent>
+          <Logo />
+
+          <div>
+            <Heading>All your favorite games in one place</Heading>
+            <S.Subtitle>
+              <strong>Won</strong> is the best and most complete gaming
+              platform.
+            </S.Subtitle>
+          </div>
+
+          <S.Footer>Won games 2023 c Todos os Direitos Reservados</S.Footer>
+        </S.BannerContent>
+      </S.BannerBlock>
+
+      <S.Content>
+        <S.ContentWrapper>
+          <Logo color="black" size="large" />
+
+          <Heading color="black" lineColor="secondary" lineLeft>
+            {title}
+          </Heading>
+          {children}
+        </S.ContentWrapper>
+      </S.Content>
     </S.Wrapper>
   )
 }
--- src/templates/Auth/test.spec.tsx
+++ src/templates/Auth/test.spec.tsx
@@ -1,13 +1,36 @@
-import { screen, render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('render test', () => {
-    const { container } = render(<Auth />)
+  it('should render logos, title, subtitle, footer and children', () => {
+    renderWithTheme(
+      <Auth title="Auth Title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', {
+        name: /All your favorite games in one place/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Won is the best and most complete gaming platform./i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Auth title/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
