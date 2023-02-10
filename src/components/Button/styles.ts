77a51e5acd851c3a1c7a291ef76c1104402e14b7
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-02-10 17:46:38 -0300

    feat: Add PaymentOptions component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -40,11 +40,17 @@ const wrapperModifiers = {
     &:hover {
       color: ${darken(0.1, theme.colors.primary)};
     }
+  `,
+  disabled: () => css`
+    &:disabled {
+      cursor: not-allowed;
+      filter: saturate(30%);
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal, disabled }) => css`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -62,9 +68,10 @@ export const Wrapper = styled.button<WrapperProps>`
         : `linear-gradient(180deg, #e35565 0%, #d958a6 50%)`};
     }
 
-    ${!!size && wrapperModifiers[size](theme)}
-    ${!!fullWidth && wrapperModifiers.fullWidth()}
-    ${!!hasIcon && wrapperModifiers.withIcon(theme)}
-    ${!!minimal && wrapperModifiers.minimal(theme)}
+    ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)};
+    ${!!minimal && wrapperModifiers.minimal(theme)};
+    ${disabled && wrapperModifiers.disabled()};
   `}
 `
