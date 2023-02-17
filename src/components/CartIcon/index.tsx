11e5cd4db15eda856e1e986244a552115138236c
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-02-17 17:43:53 -0300

    feat: Add CartIcon component

---
+++ src/components/CartIcon/index.tsx
@@ -0,0 +1,17 @@
+import { ShoppingCart } from '@styled-icons/material-outlined'
+import * as S from './styles'
+
+export type CartIconProps = {
+  quantity?: number
+}
+
+const CartIcon = ({ quantity = 0 }: CartIconProps) => {
+  return (
+    <S.Wrapper>
+      {quantity > 0 && <S.Badge aria-label="Cart Items">{quantity}</S.Badge>}
+      <ShoppingCart aria-label="Shopping cart" />
+    </S.Wrapper>
+  )
+}
+
+export default CartIcon
