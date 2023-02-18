2ad2ee416d003a7fd2a0b4ba98b94e4fb8e7abeb
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-02-18 16:16:16 -0300

    feat: Add UserDropdown component

---
+++ src/components/UserDropdown/index.tsx
@@ -0,0 +1,53 @@
+import {
+  AccountCircle,
+  FavoriteBorder,
+  ExitToApp
+} from '@styled-icons/material-outlined'
+import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown'
+
+import Dropdown from 'components/Dropdown'
+import * as S from './styles'
+import Link from 'next/link'
+
+export type UserDropdownProps = {
+  username: string
+}
+
+const UserDropdown = ({ username }: UserDropdownProps) => {
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" legacyBehavior passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
+
+        <Link href="/wishlist" legacyBehavior passHref>
+          <S.Link title="Wishlist">
+            <FavoriteBorder />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
+
+        <Link href="/logout" legacyBehavior passHref>
+          <S.Link title="Sign out">
+            <ExitToApp />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
+      </S.Nav>
+    </Dropdown>
+  )
+}
+
+export default UserDropdown
---
+++ src/components/UserDropdown/test.spec.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+  })
+
+  it('should render the menu', async () => {
+    renderWithTheme(<UserDropdown username="Willian" />)
+
+    // open menu
+    await userEvent.click(screen.getByText(/willian/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
