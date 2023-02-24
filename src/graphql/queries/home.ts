b3829103c1c039e74d0f9fdc29764ab4e9a5fb19
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-02-24 19:02:40 -0300

    feat: Add GameFragment, HighlightFragment and NewGames

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,12 +1,22 @@
 import { gql } from '@apollo/client'
 import { BannerFragment } from 'graphql/fragments/banner'
+import { GameFragment } from 'graphql/fragments/game'
 
 export const QUERY_HOME = gql`
   query QueryHome {
     banners {
       ...BannerFragment
     }
+
+    newGames: games(
+      where: { release_date_lte: "2023-02-24" }
+      sort: "release_date:desc"
+      limit: 8
+    ) {
+      ...GameFragment
+    }
   }
 
   ${BannerFragment}
+  ${GameFragment}
 `
