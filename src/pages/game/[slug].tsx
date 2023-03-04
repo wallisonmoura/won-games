413226ac5cf3caf19314843d6ca3dfc2b045ac7d
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-04 18:01:49 -0300

    fix: revalidate moving to outside and using no-cache from Apollo

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -50,7 +50,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { data } = await apolloClient.query<
     QueryGameBySlug,
     QueryGameBySlugVariables
-  >({ query: QUERY_GAMES_BY_SLUG, variables: { slug: `${params?.slug}` } })
+  >({
+    query: QUERY_GAMES_BY_SLUG,
+    variables: { slug: `${params?.slug}` },
+    fetchPolicy: 'no-cache'
+  })
 
   if (!data.games.length) {
     return { notFound: true }
@@ -74,8 +78,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   })
 
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       cover: `http://localhost:1337${game.cover?.src}`,
       gameInfo: {
         title: game.name,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,12 +17,13 @@ export async function getStaticProps() {
     data: { banners, newGames, upcomingGames, freeGames, sections }
   } = await apolloClient.query<QueryHome, QueryHomeVariables>({
     query: QUERY_HOME,
-    variables: { date: TODAY }
+    variables: { date: TODAY },
+    fetchPolicy: 'no-cache' // garantir sempre dados novos na geração dos dados
   })
 
   return {
+    revalidate: 10,
     props: {
-      revalidate: 10,
       banners: bannerMapper(banners),
       newGamesTitle: sections?.newGames?.title,
       newGames: gamesMapper(newGames),
