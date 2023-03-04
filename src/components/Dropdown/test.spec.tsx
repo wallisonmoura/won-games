8c5d24efc6abe08af8fdaad5c7d40dcc075f0a3f
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-04 19:34:34 -0300

    refactor: Add overlay to Dropdown

--- src/components/Dropdown/test.spec.tsx
+++ src/components/Dropdown/test.spec.tsx
@@ -6,7 +6,7 @@ import Dropdown from '.'
 
 describe('<Dropdown />', () => {
   beforeEach(() => {
-    const title = <h1 aria-label="toggle dropdown">Click here</h1>
+    const title = <h1 aria-label="toogle dropdown">Click here</h1>
 
     renderWithTheme(
       <Dropdown title={title}>
@@ -16,18 +16,33 @@ describe('<Dropdown />', () => {
   })
 
   it('should render title', () => {
-    expect(screen.getByLabelText(/toggle dropdown/)).toBeInTheDocument()
+    expect(screen.getByLabelText(/toogle dropdown/i)).toBeInTheDocument()
   })
 
-  it('should handle opne/close dropdown', async () => {
-    const content = screen.getByText(/content/).parentElement!
+  it('should handle open/close dropdown', async () => {
+    const content = screen.getByText(/content/i).parentElement!
 
     expect(content).toHaveStyle({ opacity: 0 })
     expect(content.getAttribute('aria-hidden')).toBe('true')
 
-    await userEvent.click(screen.getByLabelText(/toggle dropdown/))
+    await userEvent.click(screen.getByLabelText(/toogle dropdown/))
 
     expect(content).toHaveStyle({ opacity: 1 })
     expect(content.getAttribute('aria-hidden')).toBe('false')
+  })
+
+  it('should handle open/close dropdown when clicking on overlay', async () => {
+    const content = screen.getByText(/content/i).parentElement!
+    const overlay = content.nextElementSibling
+
+    await userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(overlay).toHaveStyle({ opacity: 1 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('false')
+
+    await userEvent.click(overlay!)
+
+    expect(overlay).toHaveStyle({ opacity: 0 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('true')
   })
 })
