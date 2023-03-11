af3ceeabb25d651c90574d0bbfcd8f6cad715e3b
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-11 00:11:54 -0300

    feat: Add initial structure to useCart and CartProvider with test

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,16 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning: ReactDOM.render is no longer supported in React 18./.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
