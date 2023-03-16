093dfe66d55f9aa9f36654ca72ecee00df5f3406
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-16 16:56:53 -0300

    feat: Add CartButton

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -2,11 +2,9 @@ import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 import Dropdown from 'components/Dropdown'
 
-import { useCart } from 'hooks/use-cart'
 import * as S from './styles'
 
 const CartDropdown = () => {
-  const { items, total } = useCart()
   return (
     <S.Wrapper>
       <Dropdown title={<CartIcon />}>
