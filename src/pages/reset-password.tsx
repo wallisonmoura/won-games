55b88c165b3aac9613eb330387015d97e03b0e41
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-25 18:38:47 -0300

    feat: Add ResetPassword Page and FormResetPassword

---
+++ src/pages/reset-password.tsx
@@ -0,0 +1,10 @@
+import FormResetPassword from 'components/ResetPassword'
+import Auth from 'templates/Auth'
+
+export default function ResetPassword() {
+  return (
+    <Auth title="Reset password">
+      <FormResetPassword />
+    </Auth>
+  )
+}
