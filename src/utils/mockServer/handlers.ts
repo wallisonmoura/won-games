23bb3064a6dd10a84e7628cc8e31e08c7068e6f5
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-26 14:21:17 -0300

    feat: Add tests to FormForgotPassword

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,4 +1,41 @@
 import { rest } from 'msw'
 
+type LoginReqBody = {
+  email: string
+}
+
 // onde será interceptado as chamadas
-export const handlers = []
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    async (req, res, ctx) => {
+      const { email } = req.body
+
+      // quando dar erro
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+      // quando for sucesso
+      return res(
+        ctx.status(200),
+        ctx.json({
+          email
+        })
+      )
+    }
+  )
+]
