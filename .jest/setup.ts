227a2a96985094c5fe73922e0191ffd024ba1f12
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-03-26 01:32:24 -0300

    feat: config dotenv to jest

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,5 +1,10 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: '.env.development'
+})
 
 const originalError = console.error;
 beforeAll(() => {
