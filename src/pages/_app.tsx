30724c71ede6888d80bdd0f1686f5a148d67fed0
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-04-01 20:02:18 -0300

    feat: Add mocks, methods and test structure to useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import Head from 'next/head'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { WishlistProvider } from 'hooks/use-wishlist'
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -20,19 +21,21 @@ export default function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="img/icon-512.png" />
-              <link rel="apple-touch-icon" href="img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="The best Game Stores in the world."
-              />
-            </Head>
-            <GlobalStyles />
-            <NextNProgress color="#F231A5" height={5} />
-            <Component {...pageProps} />
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="img/icon-512.png" />
+                <link rel="apple-touch-icon" href="img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world."
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNProgress color="#F231A5" height={5} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
