554eed14c222a16b500cbd0d5de32ca58e6944e3
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-04-02 00:20:06 -0300

    feat: Add remove and add to wishlistButton

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -14,7 +14,11 @@ const WishlistButton = ({
   size = 'small'
 }: WishlistButtonProps) => {
   const [session] = useSession()
-  const { isInWishlist } = useWishlist()
+  const { isInWishlist, addToWishlist, removeFromWishlist } = useWishlist()
+
+  const handleClick = () => {
+    isInWishlist(id) ? removeFromWishlist(id) : addToWishlist(id)
+  }
 
   const ButtonText = isInWishlist(id)
     ? 'Remove from Wishlist'
@@ -30,6 +34,7 @@ const WishlistButton = ({
           <FavoriteBorder aria-label={ButtonText} />
         )
       }
+      onClick={handleClick}
       minimal
       size={size}
     >
--- src/components/WishlistButton/test.spec.tsx
+++ src/components/WishlistButton/test.spec.tsx
@@ -1,5 +1,6 @@
+import userEvent from '@testing-library/user-event'
 import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
-import { render, screen } from 'utils/test-utils'
+import { act, render, screen, waitFor } from 'utils/test-utils'
 
 import WishlistButton from '.'
 
@@ -66,5 +67,41 @@ describe('<WishlistButton />', () => {
     render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
 
     expect(screen.queryByText(/remove from wishlist/i)).not.toBeInTheDocument()
+  })
+
+  it('should add to wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false,
+      addToWishlist: jest.fn()
+    }
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    act(() => {
+      userEvent.click(screen.getByText(/add to wishlist/i))
+    })
+
+    waitFor(() => {
+      expect(wishlistProviderProps.addToWishlist).toHaveBeenCalledWith('1')
+    })
+  })
+
+  it('should remove from wishlist', async () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true,
+      removeFromWishlist: jest.fn()
+    }
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    act(() => {
+      userEvent.click(screen.getByText(/remove from wishlist/i))
+    })
+
+    waitFor(() => {
+      expect(wishlistProviderProps.removeFromWishlist).toHaveBeenCalledWith('1')
+    })
   })
 })
