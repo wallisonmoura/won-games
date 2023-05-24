d67e292a37e6f7cb9d93380e24cbff6c2a826b0c
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-05-24 01:05:44 -0300

    feat: Create shouldRenderBanner custom command

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,38 +4,7 @@ describe('Home page', () => {
   it('should render home section', () => {
     cy.visit('/')
 
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .should('contain', 'Cyberpunk 2077');
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .findByRole('link', { name: /buy now/i })
-      .should('exist');
+    cy.shouldRenderBanner()
 
-
-    cy.get('.slick-dots > :nth-child(2) > button').click()
-    cy.wait(500)
-
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .should('contain', 'ELEX II');
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .findByRole('link', { name: /buy now/i })
-      .should('exist');
-
-    cy.get('.slick-dots > :nth-child(3) > button').click()
-    cy.wait(500)
-
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .should('contain', 'Horizon Zero Dawn');
-    cy.get('.slick-slider')
-      .find('.slick-active')
-      .findByRole('link', { name: /buy now/i })
-      .should('exist');
-
-    });
-
-
+  })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -40,3 +40,37 @@
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
+Cypress.Commands.add('shouldRenderBanner', () => {
+  cy.get('.slick-slider')
+      .find('.slick-active')
+      .should('contain', 'Cyberpunk 2077');
+    cy.get('.slick-slider')
+      .find('.slick-active')
+      .findByRole('link', { name: /buy now/i })
+      .should('exist');
+
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    cy.wait(500)
+
+    cy.get('.slick-slider')
+      .find('.slick-active')
+      .should('contain', 'ELEX II');
+    cy.get('.slick-slider')
+      .find('.slick-active')
+      .findByRole('link', { name: /buy now/i })
+      .should('exist');
+
+    cy.get('.slick-dots > :nth-child(3) > button').click()
+    cy.wait(500)
+
+    cy.get('.slick-slider')
+      .find('.slick-active')
+      .should('contain', 'Horizon Zero Dawn');
+    cy.get('.slick-slider')
+      .find('.slick-active')
+      .findByRole('link', { name: /buy now/i })
+      .should('exist');
+
+})
