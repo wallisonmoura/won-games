22e5eda2c245108fff159c9f52344baff6e61c69
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-05-29 11:11:18 -0300

    feat/cypress: Get GameCard inside showcase

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -85,5 +85,7 @@ Cypress.Commands.add('shouldRenderShowcase', ({name, highlight = false }) => {
         cy.findByRole('link').should('have.attr', 'href')
       })
     }
+
+    cy.get(`[data-cy="game-card"]`).should('have.length.gt', 0)
   })
 })
