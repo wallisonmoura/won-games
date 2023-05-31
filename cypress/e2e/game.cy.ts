7b85306c5d88a04f7bfa4bb070816efaa7a942b5
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-05-31 00:25:47 -0300

    feat/cypress: Get children and textContent inside Game Page

--- cypress/e2e/game.cy.ts
+++ cypress/e2e/game.cy.ts
@@ -14,5 +14,11 @@ describe('Game page', () => {
 
     // gallery
     cy.findAllByRole('button', { name: /Thumb \-/i }).should('have.length.gt', 0)
+
+    // content
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i}).should('exist')
+    })
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
   })
 })
