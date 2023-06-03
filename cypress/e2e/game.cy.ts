f346f59a382b78b10dbc4f221df8a14d21ad2dff
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-03 00:07:55 -0300

    feat/cypress: Add and Remove game from cart via Game Page

--- cypress/e2e/game.cy.ts
+++ cypress/e2e/game.cy.ts
@@ -3,7 +3,6 @@
 describe('Game page', () => {
   it('should render gape page sections', () => {
     cy.visit('/game/cyberpunk-2077')
-
     cy.getByDataCy('game-info').within(() => {
       cy.findByRole('heading', { name: /Cyberpunk 2077/i}).should('exist')
       cy.findByText(/This game is part of your Welcome Offer!/i).should('exist')
@@ -40,5 +39,35 @@ describe('Game page', () => {
 
     cy.shouldRenderShowcase({ name: "Upcoming Games", highlight: true })
     cy.shouldRenderShowcase({ name: "You may like these games", highlight: false })
+  })
+
+  it('should add/remove game in cart', () => {
+    cy.visit('/game/cyberpunk-2077')
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('button', { name: /add to cart/i }).click()
+      cy.findByRole('button', { name: /remove from cart/i }).should('exist')
+    })
+
+    cy.findAllByLabelText(/cart items/i)
+      .first()
+      .contains(1)
+      .click()
+
+    cy.getByDataCy('cart-list').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
+    })
+
+       // close dropdown
+    cy.findAllByLabelText(/cart items/i)
+    .first()
+    .click()
+
+  // remove from cart
+  cy.getByDataCy('game-info').within(() => {
+    cy.findByRole('button', { name: /remove from cart/i }).click()
+    cy.findByRole('button', { name: /add to cart/i }).should('exist')
+  })
+
+  cy.findAllByLabelText(/cart items/i).should('not.exist')
   })
 })
