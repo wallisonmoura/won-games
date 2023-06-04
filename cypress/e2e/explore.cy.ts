ca065928b00600136db84beb78c644193b227ff8
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-04 20:44:46 -0300

    feat/cypress: Add test to show more

--- cypress/e2e/explore.cy.ts
+++ cypress/e2e/explore.cy.ts
@@ -3,8 +3,11 @@
 import { priceFields, platformFields, sortFields, genreFields } from '../../src/utils/filter/fields'
 
 describe('Explore page', () => {
-  it('should render filters columns', () => {
+  beforeEach(() => {
     cy.visit('/games')
+  })
+  it('should render filters columns', () => {
+
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
     cy.findByRole('heading', { name: /platforms/i }).should('exist')
@@ -15,4 +18,11 @@ describe('Explore page', () => {
     cy.getFields(platformFields)
     cy.getFields(genreFields)
   })
+
+  it('should show 15 games and show more games when show more is clicked', () => {
+    // cy.visit('/games')
+    cy.getByDataCy('game-card').should('have.length', 15)
+    cy.findByRole('button', { name: /show more/i }).click()
+    cy.getByDataCy('game-card').should('have.length', 30)
+  });
 })
