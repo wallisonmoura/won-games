89e5b4ac7e71c76ec6215950675a5eaa05f2ee81
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-04 21:48:07 -0300

    feat/cypress: Using chainable commands - invoke, then, parse

--- cypress/e2e/explore.cy.ts
+++ cypress/e2e/explore.cy.ts
@@ -38,7 +38,11 @@ describe('Explore page', () => {
     cy.findByText(/highest to lowest/i ).click()
     cy.location('href').should('contain', 'sort=price%3Adesc')
     cy.getByDataCy('game-card').first().within(() => {
-      cy.findByText('FREE').should('not.exist')
+      cy.findByText(/^\$\d+(\.\d{1,2})?/)
+        .invoke('text')
+        .then($el => $el.replace('$', ''))
+        .then(parseFloat)
+        .should('be.gt', 0)
     })
   })
 })
