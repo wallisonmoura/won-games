0e4ca42d38e96c619b371aec2f31daeb193981fd
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-05 09:01:33 -0300

    feat/cypress: Add tests to sort by platform and genre and empty match

--- cypress/e2e/explore.cy.ts
+++ cypress/e2e/explore.cy.ts
@@ -20,10 +20,9 @@ describe('Explore page', () => {
   })
 
   it('should show 15 games and show more games when show more is clicked', () => {
-    // cy.visit('/games')
     cy.getByDataCy('game-card').should('have.length', 15)
     cy.findByRole('button', { name: /show more/i }).click()
-    cy.wait(1000)
+    cy.wait(2000)
     cy.getByDataCy('game-card').should('have.length', 30)
   })
 
@@ -44,41 +43,81 @@ describe('Explore page', () => {
 
   it('should filter by price', () => {
     cy.findByText(/highest to lowest/i).click()
-    cy.wait(1000)
+    cy.wait(2000)
     cy.findByText(/free/i).click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=0')
     cy.getByDataCy('game-card').first().within(() => {
       cy.findByText('FREE').should('exist')
     })
 
     cy.findByText('Under $50').click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=50')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeLessThan(50)
     })
 
     cy.findByText('Under $100').click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=100')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeLessThan(100)
     })
 
     cy.findByText('Under $150').click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=150')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeLessThan(150)
     })
 
     cy.findByText('Under $250').click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=250')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeLessThan(250)
     })
 
     cy.findByText('Under $500').click()
+    cy.wait(2000)
     cy.location('href').should('contain', 'price_lte=500')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeLessThan(500)
     })
+  })
+
+  it('should filter by platform and genre', () => {
+    cy.findByText(/windows/i).click()
+    cy.wait(2000)
+    cy.location('href').should('contain', 'platforms=windows')
+
+    cy.findByText(/linux/i).click()
+    cy.wait(2000)
+    cy.location('href').should('contain', 'platforms=linux')
+
+    cy.findByText(/mac os/i).click()
+    cy.wait(2000)
+    cy.location('href').should('contain', 'platforms=mac')
+
+    cy.findByText(/action/i).click()
+    cy.wait(2000)
+    cy.location('href').should('contain', 'categories=action')
+  })
+
+  it('should return empty when no games match', () => {
+    cy.visit('/games')
+    cy.findByText(/highest to lowest/i).click()
+      cy.wait(2000)
+
+    cy.findByText(/free/i).click()
+    cy.wait(2000)
+    cy.findByText(/linux/i).click()
+    cy.wait(2000)
+    cy.findByText(/sports/i).click()
+    cy.wait(2000)
+
+    cy.getByDataCy('game-card').should('not.exist')
+    cy.findByText(/We didn't find any games with this filter/i).should('exist')
   })
 })
