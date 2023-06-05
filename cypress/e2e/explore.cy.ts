f5c8f1edd8ab6694f549fa3850dbe4f7046f227b
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-04 22:35:49 -0300

    feat/cypress: Add tests to sort by price

--- cypress/e2e/explore.cy.ts
+++ cypress/e2e/explore.cy.ts
@@ -23,7 +23,7 @@ describe('Explore page', () => {
     // cy.visit('/games')
     cy.getByDataCy('game-card').should('have.length', 15)
     cy.findByRole('button', { name: /show more/i }).click()
-    cy.wait(500)
+    cy.wait(1000)
     cy.getByDataCy('game-card').should('have.length', 30)
   })
 
@@ -39,6 +39,46 @@ describe('Explore page', () => {
     cy.location('href').should('contain', 'sort=price%3Adesc')
     cy.getByDataCy('game-card').first().within(() => {
       cy.shouldBeGreaterThan(0)
+    })
+  })
+
+  it('should filter by price', () => {
+    cy.findByText(/highest to lowest/i).click()
+    cy.wait(1000)
+    cy.findByText(/free/i).click()
+    cy.location('href').should('contain', 'price_lte=0')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.findByText('FREE').should('exist')
+    })
+
+    cy.findByText('Under $50').click()
+    cy.location('href').should('contain', 'price_lte=50')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(50)
+    })
+
+    cy.findByText('Under $100').click()
+    cy.location('href').should('contain', 'price_lte=100')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(100)
+    })
+
+    cy.findByText('Under $150').click()
+    cy.location('href').should('contain', 'price_lte=150')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(150)
+    })
+
+    cy.findByText('Under $250').click()
+    cy.location('href').should('contain', 'price_lte=250')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(250)
+    })
+
+    cy.findByText('Under $500').click()
+    cy.location('href').should('contain', 'price_lte=500')
+    cy.getByDataCy('game-card').first().within(() => {
+      cy.shouldBeLessThan(500)
     })
   })
 })
