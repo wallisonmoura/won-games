34be4803faf471764ee879b691498eab67db1688
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-06-06 19:39:12 -0300

    feat/cypress: Add tests to wishlist

---
+++ cypress/e2e/wishlist.cy.ts
@@ -0,0 +1,33 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Wishlist', () => {
+  it('should add and remove games from wishlist', () => {
+    // acessa a página de wishlist não logado
+    cy.visit('/wishlist')
+
+    // redireciona e faz signIn
+    cy.signIn()
+    cy.wait(12000)
+
+    // verificar se a wishlist está vazia
+    cy.findByText(/Your wishlist is empty/i).should('exist')
+
+    // vou pegar um jogo e adicionar na wishlist
+    cy.getByDataCy('game-card').eq(0).within(() => {
+      cy.findAllByLabelText(/add to wishlist/i).click()
+    })
+
+    // verificar se o jogo está lá
+    cy.getByDataCy('wishlist').within(() => {
+      cy.getByDataCy('game-card').should('have.length', 1)
+    })
+
+    // remover este jogo
+    cy.getByDataCy('wishlist').within(() => {
+      cy.findAllByLabelText(/remove from wishlist/i).click()
+    })
+
+    // verificar se está vazio
+    cy.findByText(/Your wishlist is empty/i).should('exist')
+  })
+})
